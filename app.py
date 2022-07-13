c45fa2f9642270c896c29caacd73aad28eef0cc5
amurkhon <104159634+amurkhon@users.noreply.github.com>
2022-07-13 19:35:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,31 +10,13 @@ st.title("Fauna Classification Model")
 st.markdown("This model is not for domestic animals. Furthermore, 'Mammal' class means only human.")
 
 #Rasm joylash
-file = st.file_uploader("Load image", type=['png','jpg','gif','svg'])
+file = st.file_uploader("Load image", type=['png','jpg','gif','svg']) or st.camera_input("If you want to use a camera, You can take a picture!")
 
 if file:
     st.image(file)
 
     #PIL Convert
     img=PILImage.create(file)
-
-    #model
-    model = load_learner('animal_model.pkl')
-
-    #prediction
-    pred, pred_id, probs = model.predict(img)
-    st.success(f"Prediction: {pred}")
-    st.info(f"Probability: {probs[pred_id]*100:.1f}%")
-
-    #plotting
-    fig = px.bar(x=probs*100, y=model.dls.vocab)
-    st.plotly_chart(fig)
-file1 = st.camera_input("If you want to use a camera, You can take a picture!")
-if file:
-    st.image(file1)
-
-    #PIL Convert
-    img=PILImage.create(file1)
 
     #model
     model = load_learner('animal_model.pkl')
