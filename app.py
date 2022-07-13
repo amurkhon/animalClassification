51942c446267ebcd16b374c64e04bbae8f674b9f
amurkhon <akramjonovamurxon02@gmail.com>
2022-07-13 17:18:01 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastai.vision.all import *
+import streamlit as st
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title("Hayvonlarni klassifikatsiya qiluvchi model")
+
+#Rasm joylash
+file = st.file_uploader("Rasm yuklash", type=['png','jpg','gif','svg'])
+
+if file:
+    st.image(file)
+
+    #PIL Convert
+    img=PILImage.create(file)
+
+    #model
+    model = load_learner('animal_model.pkl')
+
+    #prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    #plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
