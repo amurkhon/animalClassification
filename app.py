1c17c4bf94cf7c51d53e12a86d6d8d95fa966ba7
amurkhon <104159634+amurkhon@users.noreply.github.com>
2022-07-13 19:17:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ plt = platform.system()
 if plt == 'Windows': pathlib.PosixPath = pathlib.WindowsPath
 
 with st.form(key='my_form'):
-username = st.text_input('Username')
-password = st.text_input('Password')
-st.form_submit_button('Login')
+    username = st.text_input('Username')
+    password = st.text_input('Password')
+    st.form_submit_button('Login')
 
 #title
 st.title("Fauna Classification Model")
